96535fb89334ee93d092e96e501c4387a5e24479
Antoine Van Luchem <antoine.van-luchem@bmw.de>
2025-02-25 15:43:30 +0100

    Add submodule

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
 import chalk from 'chalk'
 
+import { sayHello } from '@packages/child'
+
 const parent = chalk.italic.bold.underline("parent")
 console.log(`Hello from the ${parent}!`)
+
+sayHello()
